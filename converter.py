aaf8734c3217d746818646e5b853527e338cb5e1
Lotte Steenbrink <lotte@zombietetris.de>
2016-12-03 19:14:22 +0100

    converter.py: impl get_object_name()

--- converter.py
+++ converter.py
@@ -18,12 +18,31 @@ WHERE
   SERVICE wikibase:label { bd:serviceParam wikibase:language "de". }
 }
 '''
+object_name_q = '''SELECT * WHERE {
+  wd:%s rdfs:label ?labelDe.
+  FILTER(LANG(?labelDe) = "de").
+}'''
+
 saarland_q = "Q1201"
 
 population_p = "wdt:P1082"
 area_p       = "wdt:P2046"
 
 saarland_area = 0 # in square kilometers
+
+
+'''
+Get the human-readable name of a Q object (in german)
+'''
+def get_object_name(object_q):
+    query_url = api_base_url % urllib2.quote(object_name_q % object_q)
+    content = json.load(urllib2.urlopen(query_url))
+
+    try:
+        return content["results"]["bindings"][0]["labelDe"]["value"]
+    except:
+        print("ERROR: couldn't get name for object %s" % object_q)
+
 
 def get_property(object_q, prop):
     query_url = api_base_url % urllib2.quote(area_base_q % (object_q, prop))
@@ -52,6 +71,7 @@ def convert_to_saarland_area(thing):
     return result_precision
 
 if __name__ == "__main__":
+    print get_object_name("Q3012")
     print "1 Ulm sind %s Saarland" % convert_to_saarland_area("Q3012")
 
 
